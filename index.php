511cfa1f2df90e9df46c8661c31d45f1e0be80af
Noriel Achero <142378544+NorielAchero@users.noreply.github.com>
2024-01-31 19:12:14 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+   <head>
+        <title>CCL311-18</title>
+   </head>
+   <body>
+   <center>
+   <hr/>
+   <h1>BootStrap</h1>
+   <h3>Section: 3BSCS-2</h3>
+   <hr/>
+   <img src="studpic.png" width="200" height="200" style="border: 1px solid"/>
+   <p>Name: Noriel R. Achero</p>
+   <h3>BOOTSTRAP ACTIVITIES</h3>
+   
+         <a href="Activity1">Activity1</a>
+		 <br/>
+		 <a href="Activity2">Activity2</a>
+		 <br/>
+		 <a href="Activity3">Activity3</a>
+         <br/>
+		 <a href="Activity4">Activity4</a>
+         <br/>
+		 <a href="Activity5">Activity5</a>
+         <br/>
+		 <a href="Activity6">Activity6</a>
+         <br/>
+		 <a href="Portfolio">Portfolio</a>
+		 <br/>
+		 <a href="photo_gallery">Photo Gallery</a>
+		 <br/>
+	     <a href="blog_project">Blog</a>
+		 <br/>
+	     <a href="e_commerce">E-Commerce</a>
+   </body>
+   </center>
+
+</html>
