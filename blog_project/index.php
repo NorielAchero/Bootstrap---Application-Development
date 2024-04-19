b5fc4f99153b53521fb101cb0ba1adf7c06e95f0
Noriel Achero <142378544+NorielAchero@users.noreply.github.com>
2024-04-19 22:03:34 +0800

    Add files via upload

---
+++ blog_project/index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- Noriel Achero | 3 BSCS 2 -->
+    <title>My Blog</title>
+     <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <script
+    src="https://code.jquery.com/jquery-3.7.1.js"
+    integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+    crossorigin="anonymous"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    
+   
+    <link rel="stylesheet" href="css/style.css">
+
+    </head>
+    <body style = "background: silver;">
+        <nav class = "navbar navbar-custom">
+            <div class = "container-fluid">
+                <div class = "navbar-header">
+                    <a class = "navbar-brand" href="">The Blog</a>
+
+                </div>
+
+                <div class = "nav navbar-nav form-inline navbar-right" style="padding: 10px;">
+                    <div class = "input-group">
+                        <input type = "text" class = "form-control">
+                        <div class = "input-group-btn">
+                            <button class = "btn btn-default"><i class = "glyphicon glyphicon-search"></i></button>
+                        </div>
+                    </div>
+
+                </div>
+
+                <ul class = "nav navbar-nav navbar-right">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="about.php">About Us</a></li>
+                    <li><a href="post.php">Post</a></li>
+                    <li><a href="contact.php">Contact Us</a></li>
+                </ul>
+            </div>
+
+        </nav>
+
+        <header style="background: url(images/1.gif);" >
+            <div class = "text-center">
+                <h1>The Blog</h1>
+                <div class = "lead">Welcome to My Blog</div>
+            </div>
+        </header>
+
+
+        <div class="row">
+            <div class = "col-md-8 col-md-offset-2">
+                <div id="post">
+                    <h1><a href="#">Music</a></h1>
+                    <p class = "lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis condimentum quam nec augue fermentum, at congue nibh aliquam. Nulla facilisi. Proin eu diam eu lorem interdum volutpat. Fusce vulputate libero nec nulla mattis, nec fringilla eros efficitur. Mauris dignissim sem vitae magna convallis, quis aliquam lacus dignissim. Integer ultricies metus at tellus convallis, id aliquet lorem pharetra. Sed nec aliquet nisi. Vivamus non quam in risus lacinia lobortis. Sed aliquet, lorem nec dictum scelerisque, justo justo commodo sapien, nec vestibulum velit libero sit amet sem. Sed vitae tellus quis nisl feugiat rhoncus eget eget risus. Integer in eros euismod, lobortis odio vel, aliquam purus. Sed tincidunt nisl eu efficitur consequat. Sed sit amet luctus libero. Maecenas ultricies metus et velit pulvinar, vel consectetur libero faucibus. Cras efficitur turpis non aliquet hendrerit.</p>
+                    <p>April 5, 2024</p>
+                </div>
+                <div id="post">
+                    <h1><a href="#">Computer</a></h1>
+                    <p class = "lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis condimentum quam nec augue fermentum, at congue nibh aliquam. Nulla facilisi. Proin eu diam eu lorem interdum volutpat. Fusce vulputate libero nec nulla mattis, nec fringilla eros efficitur. Mauris dignissim sem vitae magna convallis, quis aliquam lacus dignissim. Integer ultricies metus at tellus convallis, id aliquet lorem pharetra. Sed nec aliquet nisi. Vivamus non quam in risus lacinia lobortis. Sed aliquet, lorem nec dictum scelerisque, justo justo commodo sapien, nec vestibulum velit libero sit amet sem. Sed vitae tellus quis nisl feugiat rhoncus eget eget risus. Integer in eros euismod, lobortis odio vel, aliquam purus. Sed tincidunt nisl eu efficitur consequat. Sed sit amet luctus libero. Maecenas ultricies metus et velit pulvinar, vel consectetur libero faucibus. Cras efficitur turpis non aliquet hendrerit.</p>
+                    <p>April 5, 2024</p>
+                </div>
+                <div id="post">
+                    <h1><a href="#">JDM Cars</a></h1>
+                    <p class = "lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis condimentum quam nec augue fermentum, at congue nibh aliquam. Nulla facilisi. Proin eu diam eu lorem interdum volutpat. Fusce vulputate libero nec nulla mattis, nec fringilla eros efficitur. Mauris dignissim sem vitae magna convallis, quis aliquam lacus dignissim. Integer ultricies metus at tellus convallis, id aliquet lorem pharetra. Sed nec aliquet nisi. Vivamus non quam in risus lacinia lobortis. Sed aliquet, lorem nec dictum scelerisque, justo justo commodo sapien, nec vestibulum velit libero sit amet sem. Sed vitae tellus quis nisl feugiat rhoncus eget eget risus. Integer in eros euismod, lobortis odio vel, aliquam purus. Sed tincidunt nisl eu efficitur consequat. Sed sit amet luctus libero. Maecenas ultricies metus et velit pulvinar, vel consectetur libero faucibus. Cras efficitur turpis non aliquet hendrerit.</p>
+                    <p>April 5, 2024</p>
+                </div>
+
+                <div class = "down-btn text-right">
+                    <button class = "btn btn-default btn-lg">Older Post</button>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class = "container-fluid">
+            <ul class = "nav navbar-nav nav-mycenter">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Technology</a></li>
+                <li><a href="#">Health</a></li>
+                <li><a href="#">Science</a></li>
+                <li><a href="#">Nature</a></li>
+            </ul>
+
+        </div>
+
+    
+  
+    </body>
+</html>
